2759a17eb9998bdd2c56cd2794592e427bdb8998
Role Rohini <123159032+RoleRohini@users.noreply.github.com>
2023-09-04 19:59:49 +0530

    Add files via upload

---
+++ random_example.py
@@ -0,0 +1,13 @@
+import random 
+if __name__=="__main__":
+
+    count =1
+    random_list=[]
+    while count < 11:
+        num = random.random()
+        random_list.append(num)
+        count = count + 1
+    print(random_list)
+
+      
+   
