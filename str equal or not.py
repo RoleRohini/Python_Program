bf5b28aa2cbed53656262d56837408deddd01e01
Role Rohini <123159032+RoleRohini@users.noreply.github.com>
2023-09-04 19:56:32 +0530

    Add files via upload

---
+++ str equal or not.py
@@ -0,0 +1,19 @@
+if __name__=="__main__":
+ 
+ #ex3 of string equal
+    string_1 = "Jack"
+    string_2 = "Jack"
+
+    if string_1 == string_2:
+        print("string are equal")
+    else:
+        print("string is not equal")
+
+ #ex4 of string  not equal
+    string_1 = "Jack"
+    string_2 = "Mack"
+
+    if string_1 == string_2:
+        print("string are equal")
+    else:
+        print("string is not equal")
