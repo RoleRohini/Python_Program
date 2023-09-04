c8745cdcf2496f79220fa763f7b0256115d744ac
Role Rohini <123159032+RoleRohini@users.noreply.github.com>
2023-09-04 19:44:04 +0530

    Add files via upload

---
+++ even.py
@@ -0,0 +1,12 @@
+if __name__=="__main__":
+    list_1=list(range(1,101))
+    list_2= []
+
+    for number in list_1:
+        if number%2==0:
+            list_2.append(number)
+    print(list_2)
+
+
+
+       
