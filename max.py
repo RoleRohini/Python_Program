8a3ca52af55c4038227909e3d3d3d431cca6c102
Role Rohini <123159032+RoleRohini@users.noreply.github.com>
2023-09-06 19:42:26 +0530

    Add files via upload

---
+++ max.py
@@ -0,0 +1,21 @@
+a = 2578
+b = 4
+
+maximum = max(a, b)
+print(maximum)
+
+
+#another method
+
+def maximum(a, b):
+	
+	if a >= b:
+		return a
+	else:
+		return b
+	
+a = 2
+b = 4
+print(maximum(a, b))
+
+
