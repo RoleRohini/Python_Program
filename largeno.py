36958fd29a5e429e9ce3d505b4c9d0372ae4cbce
Role Rohini <123159032+RoleRohini@users.noreply.github.com>
2023-09-06 19:44:03 +0530

    Add files via upload

---
+++ largeno.py
@@ -0,0 +1,12 @@
+#Program to print the largest of the three numbers. 
+a = int(input("Enter a? ")); 
+b = int(input("Enter b? ")); 
+c = int(input("Enter c? ")); 
+if a>b and a>c: 
+ print("a is largest"); 
+if b>a and b>c: 
+ print("b is largest"); 
+if c>a and c>b: 
+  print("c is largest"); 
+
+
