1185d9e38f86350e6ba8d69662b762dc566c104e
Role Rohini <123159032+RoleRohini@users.noreply.github.com>
2023-09-06 19:48:07 +0530

    Add files via upload

---
+++ voter age.py
@@ -0,0 +1,7 @@
+
+  # e 1 : Program to check whether a person is eligible to vote or not. 
+age = int (input("Enter your age? ")) 
+if age>=18: 
+  print("You are eligible to vote !!"); 
+else: 
+  print("Sorry! you have to wait !!"); 
