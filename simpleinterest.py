0055efa063768405d1c318fdceeabf71ec146b57
Role Rohini <123159032+RoleRohini@users.noreply.github.com>
2023-09-07 08:53:51 +0530

    Add files via upload

---
+++ simpleinterest.py
@@ -0,0 +1,13 @@
+
+# Python3 program to find simple interest
+def simple_interest(p,t,r):
+	print('The principal is', p)
+	print('The time period is', t)
+	print('The rate of interest is',r)
+	
+	si = (p * t * r)/100
+	
+	print('The Simple Interest is', si)
+	return si
+	
+simple_interest(8, 2, 4)
