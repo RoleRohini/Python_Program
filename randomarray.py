8271440b3c046e479f91b6339c739bd54d141023
Role Rohini <123159032+RoleRohini@users.noreply.github.com>
2023-09-09 17:30:36 +0530

    Add files via upload

---
+++ randomarray.py
@@ -0,0 +1,7 @@
+
+if __name__=="__main__":
+	random_arr=[7,4,9,6,11,35,1,5]
+	new_arr=sorted(random_arr)
+	print(random_arr)
+	print(new_arr)
+	print("using inbuilt function random of all element in an array %d"%new_arr)
