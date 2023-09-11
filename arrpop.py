4165daeeda7f25e074e0f486a5281116e4e67512
Role Rohini <123159032+RoleRohini@users.noreply.github.com>
2023-09-12 00:57:18 +0530

    Add files via upload

---
+++ arrpop.py
@@ -0,0 +1,12 @@
+
+if __name__=="__main__":
+	
+	stack_arr = [ ]
+	stack_arr.append(1)
+	stack_arr.append(2)
+	stack_arr.append(3)
+	stack_arr.append(4)
+	stack_arr.append(5)
+	print(stack_arr)
+	stack_arr.pop()
+	print(stack_arr)
