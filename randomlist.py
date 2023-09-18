fdcc55f81059154df9e3c52fe5e31dadd5cd7f14
Role Rohini <123159032+RoleRohini@users.noreply.github.com>
2023-09-18 16:24:39 +0530

    Add files via upload

---
+++ randomlist.py
@@ -0,0 +1,16 @@
+
+if __name__=="__main__": 
+    
+    random_list=[11,131,678,22,11,678,980,567,11,980]
+    uniq_list=[]
+    for number in random_list:
+        if number not in uniq_list:
+            uniq_list.append(number)
+  
+    print(random_list)
+    print(uniq_list)  
+
+    
+
+   
+  
