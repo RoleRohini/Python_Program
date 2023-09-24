ac9078e3b4f7910f593680a9c5c0908200e2bf1c
Role Rohini <123159032+RoleRohini@users.noreply.github.com>
2023-09-24 12:47:43 +0530

    Add files via upload

---
+++ vowel.py
@@ -0,0 +1,10 @@
+
+print("Enter the Character: ")
+c = input()
+
+if c=='a' or c=='e' or c=='i' or c=='o' or c=='u':
+    print("\nIt is a Vowel")
+elif c=='A' or c=='E' or c=='I' or c=='O' or c=='U':
+    print("\nIt is a Vowel")
+else:
+    print("\nIt is a Consonant")
