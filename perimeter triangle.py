5f8a95aeb74e3d4d5b496fa9d42b516c7bca698d
Role Rohini <123159032+RoleRohini@users.noreply.github.com>
2023-10-02 19:19:20 +0530

    Add files via upload

---
+++ perimeter triangle.py
@@ -0,0 +1,10 @@
+
+print("Enter the Length of First Side: ")
+a = int(input())
+print("Enter the Length of Second Side: ")
+b = int(input())
+print("Enter the Length of Third Side: ")
+c = int(input())
+
+p = a+b+c
+print("\nPerimeter = ", p)
