0673f6f8a0e7fc92297d9c52cda1240c64300e8d
Role Rohini <123159032+RoleRohini@users.noreply.github.com>
2024-08-23 16:42:30 +0530

    Add files via upload

---
+++ happy number.py
@@ -0,0 +1,20 @@
+  
+def isHappyNumber(num):    
+    rem = sum = 0;    
+        
+    while(num > 0):    
+        rem = num%10;    
+        sum = sum + (rem*rem);    
+        num = num//10;    
+    return sum;    
+               
+print("List of happy numbers between 1 and 100: ");    
+for i in range(1, 101):    
+    result = i;    
+     
+    while(result != 1 and result != 4):    
+        result = isHappyNumber(result);    
+        
+    if(result == 1):    
+        print(i),    
+        print(" "),    
