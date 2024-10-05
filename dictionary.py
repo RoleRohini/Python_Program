5bd5b5f56f0699ee66b39291127a415596744396
Role Rohini <123159032+RoleRohini@users.noreply.github.com>
2024-10-05 11:27:54 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+ 
+Dictionary = {1: 'Javatpoint', 2: 'Python', 3: 'Dictionary'}  
+print("\nDictionary created using curly braces: ")  
+print(Dictionary)  
+
+Dictionary = {'Website': 'Javatpoint', 3: [2, 3, 5, 'Dictionary']}  
+print("\nDictionary with keys of multiple data type: ")  
+print(Dictionary) 
